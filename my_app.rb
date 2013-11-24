6cffc052d07b20432c3fad96f29515b1d2014cad
Davida Marion <davidamarion@gmail.com>
2013-11-23 18:32:31 -0800

    adds cuteness.

--- my_app.rb
+++ my_app.rb
@@ -3,8 +3,12 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
  
- get '/' do
-   erb :index
- end
+  get '/' do
+    erb :index
+  end
+
+  get '/cute_pictures_of_animals' do
+    erb :cute_pictures_of_animals
+  end
 
 end
