25a90a951033dcb23e84b520a202b11e72fc431f
Davida Marion <davidamarion@gmail.com>
2013-11-26 13:15:33 -0800

    adds transparency

--- my_app.rb
+++ my_app.rb
@@ -1,30 +1,11 @@
 #my_app.rb
 require 'sinatra'
 
+
 class MyApp < Sinatra::Base
- 
-  get '/' do
-    erb :index
-  end
 
-  get '/info' do
-    erb :info
-  end
-
-  get '/work' do
-    erb :work
-  end
-
-  get '/news' do
-    erb :news
-  end
-
-  get '/contact' do
-    erb :contact
-  end
-
-  get '/cute_pictures_of_animals' do
-    erb :cute_pictures_of_animals
-  end
+    get '/' do
+      erb :index
+    end
 
 end
